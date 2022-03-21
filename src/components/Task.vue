f7a5a2d9fce311bb021b183eb4286a138e897b33
Annie Alfred <fredileader@gmail.com>
2022-03-21 11:02:58 +0100

    reminder

---
+++ src/components/Task.vue
@@ -0,0 +1,58 @@
+<template>
+<!-- The ? sysmbol means true or false and the : sysmbol means else. take note -->
+    <div @dblclick="$emit('toggle-reminder', task.id)" 
+    :class="[task.reminder ? 'reminder' : '', 'task']"
+    >
+        <h3>
+            {{ task.text }}
+            <i @click="$emit('delete-task', task.id)" class="fas fa-times"></i>
+            <!-- another way of doing the emit on the i element without using the "method
+            using this @click='$emit('delete-task', task.id)' -->
+        </h3>
+        <p>{{ task.day }}</p>
+
+    </div>
+    
+    
+</template>
+
+<script>
+export default {
+    name: 'Task',
+    props: {
+        task: Object
+    },
+    // methods: {
+    //     onDelete(id) {
+    //         this.$emit('delete-task', id)
+    //         // this is actually emiting upwards into the App.vue
+
+    //     }
+    // }
+
+}
+</script>
+
+<style scoped>
+    .fas{
+        color: red;
+    }
+    .task {
+        background: #f4f4f4;
+        margin: 5px;
+        padding: 10px 20px;
+        cursor: pointer;
+        border-radius: 5px;
+        border: 1px solid black;
+
+    }
+    .task.reminder{
+        border-left: 5px solid green;
+    }
+
+    .task h3{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+</style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,28 @@
+
+<template>
+    <div :key='task.id' v-for='task in tasks'>
+        <Task @toggle-reminder="$emit('toggle-reminder', task.id)" @delete-task="$emit('delete-task', task.id)" :task='task' />
+    </div>
+    <!-- v-for is used to create a list or we can loop through and output something -->
+    
+</template>
+
+<script>
+// dont forget to always register as a component in the export dafault
+import Task from './Task'
+
+export default {
+    name: 'Tasks',
+    props: {
+        tasks: Array
+    },
+    components: {
+        Task
+    },
+    emits: ['delete-task', 'toggle-reminder'],
+}
+</script>
+
+<style scoped>
+    
+</style>
